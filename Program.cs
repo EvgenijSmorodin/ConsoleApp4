7f54e07dba0e8d01592022740863e6a67993bcba
Евгений <@DESKTOP-SJVT4QK>
2022-05-10 13:52:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalPictures = 52;
+            int picturesAtATime = 3;
+            int totalLines;
+            int remainder;
+            totalLines = totalPictures / picturesAtATime;
+            remainder = totalPictures - totalLines*picturesAtATime;
+            Console.WriteLine($"Полностью заполненных рядов {totalLines}, а картинок сверх меры {remainder}");
+        }
+    }
+}
